4d2bf397e8608f0062d51a4ffe7a4d592cb44406
Norbert Karasek <139477824+NorbertKarasek@users.noreply.github.com>
2024-05-25 06:02:56 +0200

    Added changes to text.
    Scrolling effect and borders to smaller text.

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -4,15 +4,12 @@ import Image from "next/image";
 export default function page() {
     return (
         <div className="mypage-background h-screen flex items-center justify-center">
-            <div className="mypage-background h-screen flex items-center justify-center"></div>
-
-            <div className="z-10 flex flex-col items-center justify-center">
+            <div className="z-10 flex flex-col items-center justify-center angled-scroll">
                 <h1 className="gold-border">Norbert Karasek</h1>
-                <h1 className="text-3xl text-white font-bold"> Nr albumu: 119190</h1>
-                <p className="text-white">Strona 2</p>
-            </div>
-            <div className="text-center">
-                <p className="text-5xl text-orange-500 font-bold">Druga strona</p>
+                <h1 className="black-border"> Nr albumu: 119190</h1>
+                <p className="black-border">Praca na zaliczenie przedmiotu </p>
+                <p className="black-border">WYBRANE ŚRODOWISKA PROGRAMOWANIA </p>
+                <p className="black-border">wykonana przy użyciu framework'u Tailwind</p>
             </div>
         </div>
     );
